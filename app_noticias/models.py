647f58e85c0c353d4c7154622a640b37fd6bd55e
Laboratórios de Informática <labins@labin.canoas>
2019-03-08 21:20:00 -0300

    Primerio commit

---
+++ app_noticias/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Noticia(models.Model):
+    class Meta:
+        verbose_name = 'Notícia'
+        verbose_name_plural= 'Notícias'
+
+    titulo=models.CharField('título', max_length=128)
+    conteudo= models.TextField()
+    data_criacao=models.DateTimeField()
+    data_publicacao=models.DateTimeField()
+    publicado=models.BooleanField()
